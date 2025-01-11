7aa79c5263b25fb284b7e24ee07f4847acba5daa
Emmanuel63169 <emmanuel63169@gmail.com>
2025-01-11 13:20:02 -0700

    Forum Started

---
+++ freelancer_forum.js
@@ -0,0 +1,17 @@
+//
+
+//
+const freelancers = [
+    {name: "Amelia", Occupation: "Electrician", Price: 50},
+    {name: "Emma", Occupation: "Photograqpher", Price: 70},
+    {name: "Liam", Occupation: "Translator", Price: 45},
+    {name: "Noah", Occupation: "Writer", Price: 40},
+    {name: "Bronya", Occupation: "Illustrator", Price: 60},
+    {name: "Harper", Occupation: "Blogger", Price: 35},
+    {name: "Hector", Occupation: "Online Marketer", Price: 45},
+    {name: "Avery", Occupation: "Tutor", Price: 65},
+    {name: "Ellis", Occupation: "Voiceover Artist", Price: 50},
+    {name: "Bailey", Occupation: "Accountant", Price: 55},
+];
+
+
